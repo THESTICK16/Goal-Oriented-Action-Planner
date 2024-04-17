5013e7ebdff1d7660a9408efcb2dac2f03dc578d
Ryan <ryorke26@gmail.com>
2024-04-17 16:19:20 -0700

    Further implementation of LinkedList

--- GOAP/DataStructures/LinkedList.gd
+++ GOAP/DataStructures/LinkedList.gd
@@ -13,9 +13,8 @@ func _init(data=null):
 
 ##PRIVATE METHOD! NOT FOR EXTERNAL USE!
 ##Inserts value at the specified index in the list.
-##Assumes that checks for indexing have been made. Failure to perform checks beforehand may result in unusual behavior
 ## Returns OK if succesful, else returns an appropriate error code
-func __add(index: int, value: Variant) -> int:
+func __add(index: int, value: Variant) -> Error:
 	if index < 0 or index > _size:
 		push_error("Index ", index, " out of range for list of size ", _size)
 		return ERR_PARAMETER_RANGE_ERROR
@@ -32,8 +31,15 @@ func __add(index: int, value: Variant) -> int:
 			
 	if index == 0:
 		_head = ListNode.new(value, null, _head)
+		if _head.next != null:
+			_head.next.prev = _head
 		_size += 1
 		return OK
+		
+	#if index == size(): #FIXME IF want to make inserting at end of list instantaneous then implement this code properly. It is currently buggy
+		#var temp = _tail
+		#temp.next = ListNode.new(value, temp, null)
+		#_tail = temp.next
 		
 	var temp: ListNode = _head
 	for i in index - 1:
@@ -43,7 +49,10 @@ func __add(index: int, value: Variant) -> int:
 			push_error("Index ", index, " out of range for list of size ", _size)
 			return ERR_PARAMETER_RANGE_ERROR
 	
+	var offset_node = temp.next #Use this to set prev properly after adding the new node
 	temp.next = ListNode.new(value, temp, temp.next)
+	if offset_node != null:
+		offset_node.prev = temp.next
 	_size += 1
 	
 	while _tail.has_next():
@@ -53,9 +62,9 @@ func __add(index: int, value: Variant) -> int:
 
 ##PRIVATE METHOD! NOT FOR EXTERNAL USE!
 ##Removes and returns the value at the specified index in the list
-##Assumes that checks for indexing have been made. Failure to perform checks beforehand may result in unusual behavior
 ## Returns the value at index if succesful, else returns an appropriate error code
 func __remove(index: int) -> Variant:
+	var removed : Variant
 	if _head == null:
 		push_error("Cannot remove an element from an empty list")
 		return ERR_PARAMETER_RANGE_ERROR
@@ -65,15 +74,20 @@ func __remove(index: int) -> Variant:
 		return ERR_PARAMETER_RANGE_ERROR
 		
 	if index == 0:
+		removed = _head
 		_head = _head.next
+		if _head != null:
+			_head.prev = null
 		_size -= 1
-		return OK
+		return removed.data
 		
 	if index == _size - 1:
+		removed = _tail
 		_tail = _tail.prev
+		_tail
 		_tail.next = null
 		_size -= 1
-		return OK
+		return removed.data
 	
 	var temp: ListNode = _head
 	for i in index - 1:
@@ -82,42 +96,32 @@ func __remove(index: int) -> Variant:
 		else: 
 			push_error("Index ", index, " out of range for list of size ", _size)
 			return ERR_PARAMETER_RANGE_ERROR
-		
+	
+	removed = temp.next
 	temp.next = temp.next.next
+	temp.next.prev = temp
 	_size -= 1
 	
-	return OK
+	return removed.data
 
 ##Inserts a new element at a given position in the list. The position must be valid, or at the end of the list (pos == size()). Returns OK on success, or one of the other Error values if the operation failed.
 ##Note: This method acts in-place and doesn't return a modified list.
 ##Note: On large lists, this method will be slower if the inserted element is close to the end of the list (index 0). This is because all the list must be traversed to insert the new element.
 func insert(position: int, value: Variant) -> int:
-	#if index >= _size:
-		#return ERR_PARAMETER_RANGE_ERROR
-		#push_error("Index ", index, " out of bounds for list size ", _size)
-	#
-	#if index <= 0:
-		#_head = ListNode.new(item)
-		#_size += 1
-		#return true
-		#
-	#for i in index:
-		#pass
-	#
-		#
-	return false
+	return __add(position, value)
 
 ##Inserts all of the elements in the specified collection into this list, starting at the specified position.
-func add_all(collection, index:=-1) -> bool:
+##If no position is specified, the collection will be appended to the end
+func add_all(collection, position:=-1) -> bool:
 	return false
 
 ##Returns the first element of the list. Prints an error and returns null if the list is empty.
 func front() -> Variant:
-	return null
+	return get_item(0)
 
 ##Returns the last element of the list. Prints an error and returns null if the list is empty.
 func back() -> Variant:
-	return null
+	return get_item(_size - 1)
 	
 ##Appends the specified element to the end of this list.
 func append(value: Variant) -> bool:
@@ -150,6 +154,9 @@ func clear() -> void:
 	
 ##Returns true if this list is empty
 func is_empty() -> bool:
+	if _head == null:
+		return true
+	
 	return false
 
 ##Returns true if this list contains the specified element.
@@ -157,11 +164,39 @@ func has(value: Variant) -> bool:
 	return false
 
 ##Returns the element at the specified position in this list.
+##If negative, index is considered relative to the end of the list.
 func get_item(index: int) -> Variant:
-	return null
+	var position = index
+	if is_empty():
+		push_error("Cannot retrieve an element from an empty list")
+		return ERR_PARAMETER_RANGE_ERROR
+	
+	if index >= _size: # or index < 0:
+		push_error("Index ", index, " out of range for list of size ", _size)
+		return ERR_PARAMETER_RANGE_ERROR
+	
+	if index < 0:
+		if abs(index) > _size:
+			push_error("Index ", index, " out of range for list of size ", _size)
+			return ERR_PARAMETER_RANGE_ERROR
+		position = _size + index
+	
+	var temp = _head
+	for i in position:
+		temp = temp.next
+	
+	return temp.data
 	
 ##Returns the index of the first occurrence of the specified element in this list, or -1 if this list does not contain the element.
+##Casts all values as Strings before equality comparison in order to compare unalike types
 func index_of(value: Variant) -> int:
+	var temp = _head
+	
+	for i in _size:
+		if str(temp.data) == str(value):
+			return i
+		temp = temp.next
+		
 	return -1
 	
 ##Returns the index of the last occurrence of the specified element in this list, or -1 if this list does not contain the element.
@@ -173,10 +208,24 @@ func count(value: Variant) -> int:
 	return -1
 	
 ##Removes and returns the element of the list at index position. 
-##If negative, position is considered relative to the end of the list. 
-##Leaves the list untouched and returns null if the list is empty or if it's accessed out of bounds. An error message is printed when the list is accessed out of bounds, but not when the list is empty.
-func pop_at(position: int) -> Variant:
-	return null
+##If negative, index is considered relative to the end of the list. 
+##Leaves the list untouched and returns an error code if the list is empty or if it's accessed out of bounds. An error message is printed when the list is accessed out of bounds, but not when the list is empty.
+func pop_at(index: int) -> Variant:
+	if is_empty():
+		push_error("Cannot remove an element from an empty list")
+		return ERR_PARAMETER_RANGE_ERROR
+		
+	if index >= _size:
+		push_error("Index ", index, " out of range for list of size ", _size)
+		return ERR_PARAMETER_RANGE_ERROR
+	
+	if index < 0:
+		if abs(index) > _size:
+			push_error("Index ", index, " out of range for list of size ", _size)
+			return ERR_PARAMETER_RANGE_ERROR
+		return __remove(_size + index)
+		
+	return __remove(index)
 	
 ##Removes the first occurrence of the specified element in this list (when traversing the list from head to tail).
 func remove_first_occurence(item) -> bool:
@@ -206,6 +255,21 @@ func _to_string():
 		temp = temp.next
 		
 	list_string += str(temp) + "]"
+	
+	return list_string
+	
+func metadata():
+	var temp := _head
+	
+	if temp == null:
+		return "[]"
+		
+	var list_string: String = "["
+	while temp.has_next():
+		list_string += str(temp.metadata()) + ", "
+		temp = temp.next
+		
+	list_string += str(temp.metadata()) + "]"
 	
 	return list_string
 
@@ -267,3 +331,6 @@ class ListNode:
 		
 	func _to_string():
 		return str(data)
+		
+	func metadata():
+		return str("[Data: ", data, " Prev: ", prev, " Next: ", next, "]")
--- GOAP/Tests/UnitTests/LinkedListTest.gd
+++ GOAP/Tests/UnitTests/LinkedListTest.gd
@@ -2,9 +2,18 @@ extends UnitTest
 class_name LinkedListTest
 
 var ll: LinkedList
+var ll2: LinkedList
 
 func run_before_each():
 	ll = LinkedList.new()
+	ll2 = LinkedList.new()
+	
+	for i in 10:
+		ll2.insert(i,i)
+		
+func run_after_each():
+	test(is_list_cohesive(ll))
+	test(is_list_cohesive(ll2))
 
 func test_constructor():
 	pass
@@ -43,6 +52,7 @@ func test_add():
 	test(ll._head.data == 'd')
 	
 	test(ll.size() == 19)
+	test(is_list_cohesive(ll))
 	
 	#print(ll)
 	
@@ -54,24 +64,196 @@ func test_remove():
 	for i in 10:
 		ll.__add(i, i)
 	
-	test(OK == ll.__remove(0))
+	test(0 == ll.__remove(0))
 	test(ll._head.data == 1)
 	test(ll.size() == 9)
-	test(OK == ll.__remove(ll.size() - 1))
+	test(9 == ll.__remove(ll.size() - 1))
 	test(ll._tail.data == 8)
 	test(ll.size() == 8)
 	
-	test(OK == ll.__remove(3))
-	test(OK == ll.__remove(6))
+	test(4 == ll.__remove(3))
+	test(8 == ll.__remove(6))
+	
+	var size: int = ll.size()
 	test(ERR_PARAMETER_RANGE_ERROR == ll.__remove(-1))
+	test(ll.size() == size)
 	test(ERR_PARAMETER_RANGE_ERROR == ll.__remove(11))
+	test(ll.size() == size)
 	test(ERR_PARAMETER_RANGE_ERROR == ll.__remove(ll.size()))
+	test(ll.size() == size)
 	test(ERR_PARAMETER_RANGE_ERROR == ll.__remove(100))
+	test(ll.size() == size)
 	
 	while ll._size > 0:
-		test(OK == ll.__remove(ll.size() - 1))
-		
+		var removed = ll.__remove(ll.size() - 1)
+		#print("Removed: ", removed, " List:", ll.metadata())
+		test(is_list_cohesive(ll))
+		#test(OK == ll.__remove(ll.size() - 1))
+		#print(ll)
+		#print(ll.__remove(ll.size() - 1))
+	
 	test(ll.size() == 0)
+	
+func test_insert():
+	for i in 10:
+		ll.insert(i, i)
+		test(ll.size() == i + 1)
+		test(ll._tail.data == i)
+	
+	test(ll.to_string() == "[0, 1, 2, 3, 4, 5, 6, 7, 8, 9]")
+	
+	ll.insert(5, 'five')
+	test(ll.to_string() == "[0, 1, 2, 3, 4, five, 5, 6, 7, 8, 9]")
+	ll.insert(7, 100)
+	test(ll.to_string() == "[0, 1, 2, 3, 4, five, 5, 100, 6, 7, 8, 9]")
+
+	test(ERR_PARAMETER_RANGE_ERROR == int(ll.insert(ll.size() + 1, "blue")))
+	test(ERR_PARAMETER_RANGE_ERROR == int(ll.insert(-1, "blue")))
+	test(ERR_PARAMETER_RANGE_ERROR == int(ll.insert(50, "blue")))
+	
+	ll.insert(ll.size(), "pink")
+	test(ll._tail.data == "pink")
+	ll.insert(ll.size(), true)
+	test(ll._tail.data == true)
+	ll.insert(ll.size(), 3.21)
+	test(ll._tail.data == 3.21)
+	ll.insert(ll.size(), 'a')
+	test(ll._tail.data == 'a')
+	
+	ll.insert(0, 'b')
+	test(ll._head.data == 'b')
+	ll.insert(0, 'c')
+	test(ll._head.data == 'c')
+	ll.insert(0, 'd')
+	test(ll._head.data == 'd')
+	
+	test(ll.size() == 19)
+	test(is_list_cohesive(ll))
+	
+func test_is_empty():
+	test(ll.is_empty())
+	test(not ll2.is_empty())
+	
+	var ll3 = LinkedList.new()
+	test(ll3.is_empty())
+	ll3.insert(0, 0)
+	test(not ll3.is_empty())
+	ll3.__remove(0)
+	test(ll3.is_empty())
+	
+	test(is_list_cohesive(ll3))
+
+func test_get_item():
+	for i in 10:
+		test(i == ll2.get_item(i))
+	
+	test(ll2.get_item(ll2.size()) == ERR_PARAMETER_RANGE_ERROR)
+	test(ll2.get_item(-30) == ERR_PARAMETER_RANGE_ERROR)
+	test(ll2.get_item(50) == ERR_PARAMETER_RANGE_ERROR)
+	
+	test(ll2.get_item(0) == 0)
+	test(ll2.get_item(9) == 9)
+	test(ll2.get_item(3) == 3)
+	test(ll2.get_item(-1) == 9)
+	test(ll2.get_item(0 - ll2.size()) == 0)
+	test(ll2.get_item(-5) == 5)
+	
+func test_index_of():
+	test(ll2.index_of(9) == 9)
+	test(ll2.index_of(0) == 0)
+	test(ll2.index_of(5) == 5)
+	test(ll2.index_of(7) == 7)
+	
+	ll.insert(0, "fall")
+	ll.insert(1, "autumn")
+	ll.insert(2, true)
+	ll.insert(3, 3.14159)
+	test(0 == ll.index_of("fall"))
+	test(1 == ll.index_of("autumn"))
+	test(2 == ll.index_of(true))
+	test(3 == ll.index_of(3.14159))
+
+func test_pop_at():
+	test(ERR_PARAMETER_RANGE_ERROR == ll.pop_at(0))
+	test(ERR_PARAMETER_RANGE_ERROR == ll.pop_at(1))
+	test(ERR_PARAMETER_RANGE_ERROR == ll.pop_at(10))
+	
+	for i in 10:
+		ll.__add(i, i)
+	
+	test(0 == ll.pop_at(0))
+	test(ll._head.data == 1)
+	test(ll.size() == 9)
+	test(9 == ll.pop_at(ll.size() - 1))
+	test(ll._tail.data == 8)
+	test(ll.size() == 8)
+	
+	test(4 == ll.pop_at(3))
+	test(8 == ll.pop_at(6))
+	
+	var size: int = ll.size()
+	test(ERR_PARAMETER_RANGE_ERROR == ll.pop_at(11))
+	test(ll.size() == size)
+	test(ERR_PARAMETER_RANGE_ERROR == ll.pop_at(ll.size()))
+	test(ll.size() == size)
+	test(ERR_PARAMETER_RANGE_ERROR == ll.pop_at(100))
+	test(ll.size() == size)
+	
+	while ll._size > 0:
+		var removed = ll.pop_at(ll.size() - 1)
+		#print("Removed: ", removed, " List:", ll.metadata())
+		test(is_list_cohesive(ll))
+		#test(OK == ll.__remove(ll.size() - 1))
+		#print(ll)
+		#print(ll.__remove(ll.size() - 1))
+	
+	test(ll.size() == 0)
+	
+	#print(ll2)
+	#print(ll2.pop_at(-1))
+	#print(ll2.pop_at(0 - (ll2.size() - 1)))
+	test(9 == ll2.pop_at(-1))
+	test(0 == ll2.pop_at(0 - ll2.size()))
+	test(4 == ll2.pop_at(-5))
+	
+
+
+
 	
 func test_to_string():
 	pass
+
+
+
+
+
+func is_list_cohesive(l: LinkedList) -> bool:
+	var forward_list := []
+	var backward_list := []
+	
+	var temp = l._head
+	if temp == null:
+		return true
+	while  temp.has_next():
+		forward_list.append(temp.data)
+		temp = temp.next
+	forward_list.append(temp.data)
+		
+	temp = l._tail
+	while temp.has_prev():
+		backward_list.push_front(temp.data)
+		temp = temp.prev
+	backward_list.push_front(temp.data)
+	
+	#print("Forward_List: ", forward_list, "\nBackward List: ", backward_list) #TEST
+	
+	if forward_list.size() != backward_list.size():
+		return false
+		
+	for i in forward_list.size():
+		if forward_list[i] != backward_list[i]:
+			return false
+			
+		
+		
+	return true
